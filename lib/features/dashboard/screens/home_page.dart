07c64e43bf2d713889cc04b5f6d1bc2921d70b87
empiricdev <rtempiricinfotech@gmail.com>
2026-04-13 15:55:56 +0530

    qw

---
+++ lib/features/dashboard/screens/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:NoteNest/features/profile/profile.dart';
+
+class HomePage extends StatefulWidget {
+  final String  name;
+  final String email;
+  const HomePage({super.key,required this.name,required this.email});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _taskController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _taskController.addListener(() {
+      final String Text = _taskController.text.trim();
+      _taskController.value = _taskController.value.copyWith(
+        text: Text,
+        composing: TextRange.empty,
+      );
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Todo App',
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+
+        backgroundColor: Colors.teal[700],
+        foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Setting(name: 'raj', email: 'raj@gmail.com',)),
+              );
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ListTile(
+                  title: Text('Add Task'),
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Add Task'),
+                      content: TextField(
+                        controller: _taskController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                        onSubmitted: (value) => FontWeight.bold,
+                      ),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('cancel'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () =>
+                              Navigator.pop(context, _taskController),
+                          child: const Text('Submit'),
+                          autofocus: true,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black26,
+                            foregroundColor: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:NoteNest/features/onbording/screens/splashScreen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/services.dart';
+
+import 'features/dashboard/screens/home_page.dart';
+
+
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NoteNest',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF794098), // Primary Accent
+          surface: Colors.grey[900], // Dark Surface
+          //brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      home: MyScreen(),
+      //home: HomePage(name: '', email: '',),
+    );
+  }
+}
